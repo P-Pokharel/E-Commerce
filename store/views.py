4e48693fba230c17e701e510b192040b1499be01
P-Pokharel <pratyush.pokharel77@gmail.com>
2024-02-10 20:44:28 +0545

    Setting totals cost and items in Cart

--- store/views.py
+++ store/views.py
@@ -20,8 +20,15 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {
+            'get_cart_total': 0,
+            'get_cart_items': 0,
+        }
 
-    context = {'items': items}
+    context = {
+        'items': items,
+        'order': order,
+    }
     return render(request, template_name, context)
 
 def checkout(request):
