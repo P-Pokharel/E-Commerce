db5693ddfb2b80093fd8f159f3602f0b38ffe28e
P-Pokharel <pratyush.pokharel77@gmail.com>
2024-02-11 18:37:13 +0545

    Setting up the event handler for add to cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+var updateBtns = document.getElementsByClassName("update-cart");
+for(var i = 0 ; i < updateBtns.length ; i++){
+    updateBtns[i].addEventListener("click", function(){
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log("ProductID: ", productId, "Action: ", action);
+
+        console.log("USER: ", user);
+        if(user === "AnonymousUser"){
+            console.log("Not logged in.");
+        } else {
+            console.log("User is logged in.");
+        }
+    })
+}
